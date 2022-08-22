a851d55e05d731720fa36665c6600ccae026df22
Haccien <h4ccien@gmail.com>
2022-08-22 18:01:30 -0400

    Initial Commit

---
+++ RingerAlpha/RingerAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingerAlphaApp.swift
+//  RingerAlpha
+//
+//  Created by Ethan Porter on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RingerAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
